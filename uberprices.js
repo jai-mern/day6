e32ecfede527064327f5d6ef3fe4d7635f6d68ed
Jayakanth J <145441150+jai-mern@users.noreply.github.com>
2023-09-28 19:43:30 +0530

    Add files via upload

---
+++ uberprices.js
@@ -0,0 +1,8 @@
+class uber{
+    constructor(foodname,foodprices){
+        this.foodname=foodname;
+        this.foodprices=foodprices;
+    }
+}
+var c=new uber("pongal","30",);
+console.log(c.foodname);
