8b367b58fad50a1559befc484d433114133d12bb
Jayakanth J <145441150+jai-mern@users.noreply.github.com>
2023-09-28 19:42:34 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,10 @@
+class person{
+    constructor(name,year,age,address){
+        this.name=name;
+        this.year=year;
+        this.age=age;
+        this.address=address;
+    }
+}
+var c=new person("jai","1994","29","villupuram",);
+console.log(c.name);
